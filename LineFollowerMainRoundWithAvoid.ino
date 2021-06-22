c26915b2a572fb811d72dccec3f41031fd55053e
Imrul2322 <imrul007@iut-dhaka.edu>
2021-06-21 23:08:28 -0500

    Add files via upload

---
+++ LineFollowerMainRoundWithAvoid.ino
@@ -0,0 +1,420 @@
+//go
+#define trigPin 33
+#define echoPin 31
+/////////////////////////////////////////////////////////////////
+#include "MotorClassL.h"
+int pinArray[]={30,28,26,24,7,8};
+MotorClassL mc(pinArray);
+
+int ML=150,MR=150;
+int MLx=200,MRx=200;
+int mlSpeed,mrSpeed;
+//////////////////////////////////////////////////////////////////
+#include <QTRSensors.h>
+#define NUM_SENSOR    8
+#define TIMEOUT       2500
+#define EMITTER_PIN   2
+//#define SENSOR_DEBUG
+#define SONAR_DEBUG
+//#define CALIBON
+//int sensorPin[]={34,36,38,40,42,44,46,48};
+int sensorPin[]={48,46,44,42,40,38,36,34};
+QTRSensorsRC qtrrc((unsigned char[]) {48,46,44,42,40,38,36,34},
+  NUM_SENSOR, TIMEOUT, EMITTER_PIN); 
+  
+//int chk[NUM_SENSOR];
+unsigned int sensorD[NUM_SENSOR];
+unsigned int sensorA[NUM_SENSOR];
+int sensorMin[NUM_SENSOR];
+int sensorMax[NUM_SENSOR];
+int sensorExPin[]={A8,A9,A10,A11};
+int chke[4];
+int sensorExD[4];
+//////////////////////////////////////////////////////////////////
+int lastError=0;
+float KP,KD;
+boolean isMotorEnabled;
+//////////////////////////////////////////////////////////////////
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  sensorInit();
+  isMotorEnabled=true;
+  mlSpeed = mrSpeed = 0;
+  ML=MR=160;
+  KP=.93+.03;//.89
+  KD=1.75+.04; //1.68
+}
+
+void loop(){
+  
+  //motorTest();
+  //digitalWrite(22,HIGH);
+  //analogWrite(9,100);
+  //mc.motor2(0,1,100);
+  //sensorTest();
+  /*int x=1;
+  x=analogRead(A8);
+  Serial.print(x);
+  delay(100);*/
+  
+  
+  //mc.motorForward(ML,MR);
+  fire();
+  pid();
+  patternCheck();
+  pidCalib();
+  Serial.println();/**/
+  
+  //avoidRight();
+}
+int patternCheck(){
+  //if(chk[0]&&chk[1]&&chk[2]&&chk[3]&&chk[4]&&chk[5]) ;
+  if(sensorD[0]&&sensorD[1]&&sensorD[2]&&sensorD[3]&&!sensorD[7]&&sensorD[4])    //111000
+  {
+    mc.motorForward(ML,MR);
+    delay(100+20);
+    if(sensorReadEx){
+      turnLeftMod();
+      //turnLeft();
+    }
+    else turnLeft();
+  }
+  else if(sensorD[7]&&sensorD[6]&&sensorD[5]&&sensorD[4]&&!sensorD[0]&&sensorD[3])   //000111
+  {
+    mc.motorForward(ML,MR);
+    delay(100+20);
+    if(sensorReadEx){
+      turnRightMod();
+      //turnRight();
+    }
+    else turnRight();
+  }
+  else if(!sensorD[0]&&!sensorD[1]&&!sensorD[2]&&!sensorD[3]&&!sensorD[4]&&!sensorD[5]&&!sensorD[6]&&!sensorD[7]) {
+    mc.motorForward(ML,MR);
+    delay(100+20);
+  }
+  /*else if(chk[0]&&chk[1])left();   //110000
+  else if(chk[5]&&chk[4])right();    //000011*/
+  
+  //else if(!chk[0]&&!chk[1]&&!chk[2]&&!chk[3]&&!chk[4]&&!chk[5]) reverse(); 
+}
+
+void motorTest(){
+ // mc.motorForward(200,200);
+// turnRight();
+ turnLeft();
+  //delay(1500);
+  mc.brake();
+  delay(1500);
+}
+void sensorTest(){
+  sensorRead();
+}
+void pidCalib(){
+  if(Serial.available()){
+    char x=Serial.read();
+    if(x=='+'){
+      if(KP<3)KP+=.01;
+    }
+    else if(x=='-'){
+      if(KP>0)KP-=.01;
+    }
+    else if(x=='9'){
+      if(KD<3)KD+=.01;
+    }
+    else if(x=='0'){
+      if(KD>0)KD-=.01;
+    }
+    else if(x=='w'||x=='W'){
+      if(ML<250){
+        ML+=5;
+        MR+=5;
+      }
+    }
+    else if(x=='s'||x=='S'){
+      if(ML>0){
+        ML-=5;
+        MR-=5;
+      }
+    }
+  }
+}
+void pid(){
+  int pos=sensorRead(); //range: 800 to 6400
+  int error = ((double)pos/8-450)*250/350; //range: -250 to 250
+  #ifdef SENSOR_DEBUG
+  Serial.print('\t');
+  Serial.print(error);
+  Serial.print('\t');
+  Serial.print(KP);
+  Serial.print('\t');
+  Serial.print(KD);
+  #endif
+  //int memory = (lastError - error);
+  int memory = (error - lastError);
+  int motorSpeed = KP * error + KD * memory;
+  lastError = error;
+  
+  mlSpeed=ML;
+  mrSpeed=MR;
+  int maxSpeed=230;
+  if((ML+motorSpeed) > maxSpeed){
+    mlSpeed=maxSpeed;
+    mrSpeed=MR-motorSpeed-(ML+motorSpeed-maxSpeed);
+  }
+  else if((MR-motorSpeed) > maxSpeed){
+    mrSpeed=maxSpeed;
+    mlSpeed=ML+motorSpeed-(MR-motorSpeed-maxSpeed);
+  }
+  else{
+    mlSpeed=ML+motorSpeed;
+    mrSpeed=MR-motorSpeed;
+  }
+  
+  /*if(motorSpeed<0) mlSpeed = ML + motorSpeed;
+  else if(motorSpeed>0) mrSpeed = MR - motorSpeed;
+  else {
+    mlSpeed=MLx;
+    mrSpeed=MRx;
+  }
+  */
+  if (mlSpeed < 0)
+    mlSpeed = 0;
+  if (mrSpeed < 0)
+    mrSpeed = 0;
+    
+   Serial.print('\t');
+   Serial.print(mlSpeed);
+   Serial.print('\t');
+   Serial.print(mrSpeed);
+   Serial.print('\t');
+   Serial.print(ML);
+   
+   if(isMotorEnabled) mc.motorForward(mrSpeed,mlSpeed);
+}
+
+void turnLeft()
+{
+  
+    //mc.motorForward(m1Speed,m2Speed);
+   // mc.motorForward(M1,M2);
+    mc.brake();
+    delay(50);
+    while(1)
+    {
+      sensorRead();
+      left();
+      if(sensorD[1] || sensorD[2] || sensorD[3] )break;
+    }
+}
+void turnRight()
+{
+  
+    //mc.motorForward(M1,M2);
+    //delay(100-20);
+    mc.brake();
+    delay(50);
+    while(1)
+    {
+      sensorRead();
+      right();
+      if(sensorD[4] || sensorD[5] || sensorD[6]) break;
+    }
+}
+void turnLeftMod()
+{
+  
+    //mc.motorForward(m1Speed,m2Speed);
+   // mc.motorForward(M1,M2);
+    mc.brake();
+    delay(50);
+    while(1){
+      sensorRead();
+      left();
+      if(!sensorD[1] && !sensorD[2] && !sensorD[3] )break;
+    }
+    while(1)
+    {
+      sensorRead();
+      left();
+      if(sensorD[3] )break;
+    }
+    mc.brake();
+    delay(500);
+}
+void turnRightMod()
+{
+  
+    //mc.motorForward(M1,M2);
+    //delay(100-20);
+    mc.brake();
+    delay(50);
+    while(1){
+      sensorRead();
+      right();
+      if(!sensorD[4] && !sensorD[5] && !sensorD[6])break;
+    }
+    while(1)
+    {
+      sensorRead();
+      right();
+      if(sensorD[4] ) break;
+    }
+    mc.brake();
+    delay(500);
+}
+void left()
+{
+  if(isMotorEnabled)mc.motorForward(-160+40,160-40);
+}
+void right()
+{
+  if(isMotorEnabled)mc.motorForward(160-40,-160+40);
+}
+long fire()
+{
+  long duration, distance;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(1000); - Removed this line
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH, 30000);
+  distance = (duration/2) / 29.1;
+  
+  if(distance>0 && distance<10){
+    mc.brake();
+    isMotorEnabled=false;
+    avoidRight();
+  }
+  else isMotorEnabled=true;
+  
+  if (distance >= 200 || distance <= 0){
+    //isMotorEnabled=false;
+    //nothing
+  }
+  
+  #ifdef SONAR_DEBUG
+  Serial.print(distance);
+  Serial.println(" cm");
+  #endif
+  return distance;
+}
+
+void avoidRight1(){
+  int unitDelay=10;
+  int mfl=100,mfr=117,mtl=100,mtr=105;
+  int turnLoop=80,ForwardShortLoop=100,ForwardLongLoop=25;
+  mc.brake();
+  delay(1000);
+  for(int i=0;i<turnLoop;i++){ ///////right
+    mc.motorForward(mtl,-mtr);
+    delay(unitDelay);
+  }
+  mc.brake();
+  delay(300);
+  for(int i=0;i<ForwardShortLoop;i++){ ///////forward small
+    mc.motorForward(mfl,mfr);
+    delay(unitDelay);
+  }
+  mc.brake();
+  delay(300);
+  for(int i=0;i<turnLoop;i++){///////left
+    mc.motorForward(-mtl,mtr);
+    delay(unitDelay);
+  }
+  mc.brake();
+  delay(300);
+  for(int i=0;i<ForwardLongLoop;i++){///////forward large
+    mc.motorForward(mfl,mfr);
+    delay(unitDelay*10);
+  }
+  mc.brake();
+  delay(300);
+  for(int i=0;i<turnLoop;i++){///////left
+    mc.motorForward(-mtl,mtr);
+    delay(unitDelay);
+  }
+  mc.brake();
+  delay(300);
+  for(int i=0;i<ForwardShortLoop;i++){///////forward small
+    mc.motorForward(mfl,mfr);
+    //if(sensorD[7])break;
+    delay(unitDelay);
+  }
+  mc.brake();
+  delay(100);
+  while(1){
+    mc.motorForward(mfl,mfr);
+    if(sensorD[7])break;
+  }
+  mc.brake();
+  delay(1000);
+  for(int i=0;i<turnLoop;i++){ ///////right
+    mc.motorForward(mtl,-mtr);
+    delay(unitDelay);
+  }
+  mc.brake();
+  delay(1000);
+}
+
+void avoidRight(){
+  int unitDelay=10;
+  int mfl=100,mfr=117,mtl=100,mtr=105;
+  int turnLoop=80,ForwardShortLoop=100,ForwardLongLoop=25;
+  mc.brake();
+  delay(1000);
+  for(int i=0;i<turnLoop;i++){ ///////right
+    mc.motorForward(mtl,-mtr);
+    delay(unitDelay);
+  }
+  mc.brake();
+  delay(300);
+  for(int i=0;i<ForwardShortLoop;i++){ ///////forward small
+    mc.motorForward(mfl,mfr);
+    delay(unitDelay);
+  }
+  mc.brake();
+  delay(300);
+  for(int i=0;i<turnLoop;i++){///////left
+    mc.motorForward(-mtl,mtr);
+    delay(unitDelay);
+  }
+  mc.brake();
+  delay(300);
+  for(int i=0;i<ForwardLongLoop;i++){///////forward large
+    mc.motorForward(mfl,mfr);
+    delay(unitDelay*10);
+  }
+  mc.brake();
+  delay(300);
+  for(int i=0;i<turnLoop;i++){///////left
+    mc.motorForward(-mtl,mtr);
+    delay(unitDelay);
+  }
+  mc.brake();
+  delay(300);
+  for(int i=0;i<ForwardShortLoop+10;i++){///////forward small
+    mc.motorForward(mfl,mfr);
+    //if(sensorD[7])break;
+    delay(unitDelay);
+  }
+  mc.brake();
+  delay(100);
+  while(1){
+    mc.motorForward(mfl,mfr);
+    if(sensorD[7]||sensorD[6]||sensorD[5]||sensorD[4])break;
+  }
+  mc.brake();
+  delay(1000);
+  for(int i=0;i<turnLoop;i++){ ///////right
+    mc.motorForward(mtl,-mtr);
+    delay(unitDelay);
+  }
+  mc.brake();
+  delay(500);
+}
---
+++ sensor.ino
@@ -0,0 +1,112 @@
+void sensorInit(){
+  int i;
+  for(i=0;i<4;i++){
+    pinMode(sensorExPin[i],INPUT);
+  }
+  //sensorCalib(10);
+  sensorCalibMan();
+  for (i = 0; i < NUM_SENSOR; i++){
+    Serial.print(qtrrc.calibratedMinimumOn[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+  for (i = 0; i < NUM_SENSOR; i++){
+    Serial.print(qtrrc.calibratedMaximumOn[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+  Serial.println();
+}
+void sensorCalib(int sec){
+  int i,j;
+  pinMode(13,OUTPUT);
+  int light=HIGH;
+  for (i = 0; i < sec; i++) 
+  {
+    digitalWrite(13,light);
+    light=(light==HIGH)?LOW:HIGH;
+    for (j = 0; j < 40; j++) //40 loops for 1 sec apprx
+    {
+      qtrrc.calibrate();
+    }
+  }
+}
+void sensorCalibMan(){
+  int i;
+  qtrrc.calibrate();
+  //int cmin[]={840,676,736,344,792,568,668,788};
+  //int cmin[]={732,676,844,400,960,676,732,844};
+  //int cmin[]={288,236,340,120,236,120,124,120};
+  int cmin[]={348,284,292,120,412,236,292,348};
+  int cmax[]={2500,2348,2500,1396,2500,2460,2500,2500};
+  for (i = 0; i < NUM_SENSOR; i++){
+    qtrrc.calibratedMinimumOn[i]=cmin[i];
+  }
+  for (i = 0; i < NUM_SENSOR; i++){
+    qtrrc.calibratedMaximumOn[i]=cmax[i];
+  }
+}
+int sensorRead(){
+  unsigned int position = qtrrc.readLine(sensorA);
+  unsigned int pos;
+  int threshold=800;
+  int sum=0,avg=0;
+  static int prevpos;
+  unsigned char i;
+  
+  
+  for (i = 0; i < NUM_SENSOR; i++)
+  {
+    if((sensorA[i] * 10 / 1001)<=1){
+      sensorD[i]=1;
+    }
+    else sensorD[i]=0;
+    avg+=(sensorD[i]*threshold*(i+1));
+    sum+=sensorD[i];
+    #ifdef SENSOR_DEBUG
+    //Serial.print(sensorA[i] * 10 / 1001);
+    Serial.print(sensorD[i]);
+    Serial.print('\t');
+    #endif
+  }
+  if(sum==0){
+    if(prevpos> ( (NUM_SENSOR+1)*threshold/2 ) ){
+      pos=NUM_SENSOR*threshold;
+    }
+    else pos=threshold;
+  }
+  else pos=avg/sum;
+  prevpos=pos;
+  #ifdef SENSOR_DEBUG
+  Serial.print("\t");
+  Serial.print(pos);
+  #endif
+  return pos;
+}
+
+boolean sensorReadEx()
+{
+  /*for(int i=8,j=0;i<=11;i++,j++)
+  {
+    chke[j]=analogRead(i);
+    Serial.print(chke[j]);
+    Serial.print(" ");
+  }
+  */
+  
+  
+  for(int j=0;j<4;j++)
+  {
+    chke[j]=analogRead(sensorExPin[j]);
+    //chke[j]=analogRead(i);
+    //Serial.print(chke[j]);
+    //Serial.print(" ");
+    if(chke[j]<200) sensorExD[j]=1;
+    else sensorExD[j]=0;
+    //Serial.print(sensorExD[j]);
+    //Serial.print(" ");
+  }
+  if(sensorExD[1]&&sensorExD[2]) return true;
+  else return false;
+  //Serial.println(" ");
+}
